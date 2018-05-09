5e30061f7db39f9d409c5585d1186fd10ac9b926
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2018-05-09 20:24:33 -0300

    Service - Abstrac (Alterado, Adicionado) Alterado o metodo vibializaDataInicial(); Adicionado o metodo vibializaDataFinal();

--- src/main/java/br/com/joao/lojavirtual3/util/service/ServiceAbstract.java
+++ src/main/java/br/com/joao/lojavirtual3/util/service/ServiceAbstract.java
@@ -2,13 +2,23 @@ package br.com.joao.lojavirtual3.util.service;
 
 import java.util.Date;
 
-import br.gov.go.agr.ouvidoria.exception.NegocioException;
+import br.com.joao.lojavirtual3.exception.NegocioException;
+import br.com.joao.lojavirtual3.util.date.DateUtil;
+
 
 public abstract class ServiceAbstract {
 
-	public void vibializaDataInicial(Date dataInicial) {
-		if (dataInicial == null) throw new NegocioException("Data Inicial OBRIGATÓRIA!");
+	public Date vibializaDataInicial(Date dataInicial) throws NegocioException  {
+		if (dataInicial == null)
+			throw new NegocioException("Data Inicial OBRIGATÓRIA!");
 		else
 			return dataInicial;
+	}
+
+	public Date vibializaDataFinal(Date dataInicial, Date dataFinal) throws NegocioException {
+		if (DateUtil.dataFinalEAntesDaDataInicial(dataInicial, dataFinal))
+			throw new NegocioException("Data Inicial anterior a Data Final OBRIGATÓRIA!");
+		else
+			return dataFinal;
 	}
 }
