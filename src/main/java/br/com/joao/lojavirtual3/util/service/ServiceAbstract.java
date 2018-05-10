7ed0a86e85a662c5c3aa069a84dc182da7a2b935
joao <joao@UB-L-G50-80>
2018-05-10 17:38:35 -0300

    Commit correca

--- src/main/java/br/com/joao/lojavirtual3/util/service/ServiceAbstract.java
+++ src/main/java/br/com/joao/lojavirtual3/util/service/ServiceAbstract.java
@@ -2,13 +2,18 @@ package br.com.joao.lojavirtual3.util.service;
 
 import java.util.Date;
 
-import br.gov.go.agr.ouvidoria.exception.NegocioException;
+import br.com.joao.lojavirtual3.exception.NegocioException;
 
 public abstract class ServiceAbstract {
 
-	public void vibializaDataInicial(Date dataInicial) {
+	public Date vibializaDataInicial(Date dataInicial) throws NegocioException {
 		if (dataInicial == null) throw new NegocioException("Data Inicial OBRIGATÓRIA!");
 		else
 			return dataInicial;
+	}
+
+	public void vibializaDataFinal(Date dataInicial, Date dataFinal) {
+		// TODO Auto-generated method stub
+		
 	}
 }
