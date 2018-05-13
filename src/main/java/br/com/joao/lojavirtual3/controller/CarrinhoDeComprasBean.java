d3c7122516b05ebbc7ecd742c294285a0b3b0992
joao <joao@UB-L-G50-80>
2018-05-12 22:51:03 -0300

    Criado o controller do carrinho de compras

---
+++ src/main/java/br/com/joao/lojavirtual3/controller/CarrinhoDeComprasBean.java
@@ -0,0 +1,15 @@
+package br.com.joao.lojavirtual3.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class CarrinhoDeComprasBean implements Serializable {
+
+	private static final long serialVersionUID = -4216482421562681132L;
+	
+}
