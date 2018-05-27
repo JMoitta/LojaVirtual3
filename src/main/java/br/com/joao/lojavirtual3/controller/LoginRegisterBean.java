30ce02b317ae10c718f47f127b8b599c76d3f362
joao <joao@UB-L-G50-80>
2018-05-27 07:59:20 -0300

    Criado Bean LoginRegister

---
+++ src/main/java/br/com/joao/lojavirtual3/controller/LoginRegisterBean.java
@@ -0,0 +1,28 @@
+package br.com.joao.lojavirtual3.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.joao.lojavirtual3.model.FormaPgto;
+import br.com.joao.lojavirtual3.model.ItensPedido;
+import br.com.joao.lojavirtual3.model.Pedido;
+import br.com.joao.lojavirtual3.model.Produto;
+import br.com.joao.lojavirtual3.service.CarrinhoDeComprasService;
+import br.com.joao.lojavirtual3.service.FormaPgtoService;
+import br.com.joao.lojavirtual3.util.seguranca.Seguranca;
+
+@ManagedBean
+@ViewScoped
+public class LoginRegisterBean implements Serializable {
+
+	private static final long serialVersionUID = -8999210582086851446L;
+
+	
+}
